37180b81f6c74e087b617e33a49688b50c59e1c7
Maria Cristina <MAC@macrissroberts.local>
2024-08-31 16:08:17 -0500

    primer intento

---
+++ Crisroberts1613.R
@@ -0,0 +1,11 @@
+>library ("BarcodingR")
+>install.packages("BarcodingR")
+>install.packages("BarcodingR")
+install.packages("SPlits")
+library(SPlits)
+library(paran)
+install.packages("splits",repos="http://R-Forge.R-project.org")
+library(splits)
+library("paran")
+install.packages("splits",repos="http://R-Forge.R-project.org")
+
